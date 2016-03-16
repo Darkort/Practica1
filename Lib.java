650dda5da869d3ca05659552a6eeff1901e5b25c
Dayann23 <dayawendy@hotmail.com>
2016-03-17 00:12:07 +0100

    conflicto resuelto

--- Lib.java
+++ Lib.java
@@ -12,9 +12,12 @@ public class Lib {
 
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("modificado por alumno 2"); // alumno 2
+
     }
 
     public void metodoComunitario(){
+
         System.out.println("Aqui escribimos todos");
 // modificaciones alumno 1
         System.out.println("Alumno 1 inserta esta sentencia");
